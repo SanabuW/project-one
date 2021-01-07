0bb8f0111e9da449ba60330e34ca85820ed918a3
Sanabu W <sanabuwashizuka@gmail.com>
2021-01-06 19:40:50 -0800

    added some more code to conflict tester

--- CONFLICT_TESTER.py
+++ CONFLICT_TESTER.py
@@ -19,3 +19,30 @@ list1 = ["a","b","c","d","e"]
 
 print(list1[1])
 
+'''
+A BUNCH OF COMMENTED CODE HERE 2nd SECTION
+
+SOME RANDOM TEXT
+qwertyuiop
+
+fdshajk;fadhs
+
+
+dklfjdk
+dfjkfdkfdfjkl
+
+'''
+
+print(list1[3])
+
+
+#SW - added onto after 7:36 PM PST
+list2 = ["apple", "banana", "orange"]
+
+list2[2]
+
+'''
+
+some more test code
+
+'''
